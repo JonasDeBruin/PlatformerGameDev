6f5909c21db5002df16575c036ab4a1c49bc1501
Jonas de Bruin <91885896+JonasDeBruin@users.noreply.github.com>
2022-11-17 12:45:12 +0100

    Added Jump

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -1,12 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Conponents
+    private Rigidbody2D rb2d;
+
+    //MOVEMENT
     public float speed = 10;
     float dirX;
-    private Rigidbody2D rb2d;
+
+    //Jumping
+    float jump;
+    float jumpTime = 0.5f;
+    public float maxJumpTime = 0.5f;
+    public bool grounded;
+    bool doubleJump = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +26,49 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DirectionalMovement();
+        Jumping();
+    }
+
+    void DirectionalMovement()
+    {
         dirX = Input.GetAxis("Horizontal");
         transform.Translate(transform.right * dirX * speed * Time.deltaTime);
     }
+
+
+    void Jumping()
+    {
+        jump = Input.GetAxis("Jump");
+        if (jump == 1)
+        {
+            jumpTime -= Time.deltaTime;
+
+            Debug.Log(jumpTime);
+            Debug.Log(doubleJump);
+
+            if (jumpTime > 0)
+            {
+                rb2d.AddForce(transform.up, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            grounded = true;
+            jumpTime = maxJumpTime;
+            doubleJump = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D Collider)
+    {
+        if (Collider.gameObject.layer == 6)
+        {
+            grounded = false;
+        }
+    }
+
 }
